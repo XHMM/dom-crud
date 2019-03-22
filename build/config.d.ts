fa6954dd118d497bb80876862e482997a977e4c5
xhmm <super.xhmm@gmail.com>
2019-03-22 22:54:43 +0800

    fix empty build files

--- build/config.d.ts
+++ build/config.d.ts
@@ -4,8 +4,10 @@ interface ICrudConfig {
             beforeScript: boolean;
         };
     };
+    debug: boolean;
 }
+declare let _console: Console;
 declare function getCrudConfig(): ICrudConfig;
 declare function updateCrudConfig(newConfig: Partial<ICrudConfig>): void;
 declare function readConfigByKey(key: string): any;
-export { getCrudConfig, updateCrudConfig, readConfigByKey };
+export { getCrudConfig, updateCrudConfig, readConfigByKey, _console };
--- build/crud.d.ts
+++ build/crud.d.ts
@@ -2,5 +2,5 @@ declare function cdom(tagName: string, ...options: Array<string | Record<string,
 declare function rdom<E extends Element = Element>(selector: string): E | null;
 declare function rdoms<E extends Element = Element>(selector: string): NodeListOf<E>;
 declare function udom($dom: HTMLElement, ...options: Array<string | Record<string, any>>): HTMLElement;
-declare function ddom($dom: HTMLElement): void;
+declare function ddom($dom: HTMLElement | null): boolean;
 export { cdom, rdom, rdoms, udom, ddom };
